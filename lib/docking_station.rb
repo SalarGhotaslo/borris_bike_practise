19041b8bd937fa28b94ffedf42add1b1fd8ab56d
Salar Ghotaslo <Salar_@live.co.uk>
2021-01-08 15:58:05 +0000

    step 14 completed

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,18 +1,24 @@
 require_relative 'bike.rb'
 
 class DockingStation
-    attr_reader :bike
+    attr_reader :bikes
+
+ def initialize
+    @bikes = []
+end
+
+
     def release_bike
-        fail 'No bikes available' unless @bike
-        @bike
+        fail 'No bikes available' if @bikes.empty?
+        @bikes 
     end
 
     def dock(bike)
-        fail 'Docking station full' if @bike
-        @bike = bike
+        fail 'Docking station full' if @bikes.length >= 20
+        @bikes << bike
       end
     
     def bike
-       @bike
+       @bikes
     end
 end
